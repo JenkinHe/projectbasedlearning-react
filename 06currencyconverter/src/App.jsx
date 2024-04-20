0a7a93aa4287f12740b5480d7c758fb74c15ad46
Jenkibear <jenkinwork123@gmail.com>
2024-04-20 19:59:31 +1000

    convert and swap functions

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -2,9 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import useCurrencyInfo from './hooks/useCurrencyInfo';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount,setAmount] =useState(0);
+  const [from,setFrom] =useState('usd');
+  const [to,setTo]= useState('inr');
+  const [convertedAmount,setConvertedAmount] = useState(0)
+
+  const currencyInfo=useCurrencyInfo(from);
+  const options =Object.keys(currencyInfo);
+
+  function swap(){
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+
+  }
+
+  function convert(){
+    setConvertedAmount(amount*currencyInfo[to]);
+  }
 
   const imageurl='https://www.allkpop.com/upload/2023/07/content/231619/web_data/allkpop_1690144054_hanni.jpg'
 
