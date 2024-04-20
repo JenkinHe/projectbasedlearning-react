26002f737ba834f29d80aab2efba20eaecadfec6
Jenkibear <jenkinwork123@gmail.com>
2024-04-20 21:49:42 +1000

    boiler plate header plus some routing nested routing link etc

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter,createRoutesFromElements } from 'react-router-dom'
+import Layout from './layout.jsx'
+import Home from './components/Home/home.jsx'
+import About from './components/about/About.jsx'
+
+const router =createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+
+  </Route>
+  ),
+  
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
