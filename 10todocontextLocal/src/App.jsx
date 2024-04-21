4d5e1c0900cc71d1592f9a65a11f4e4758626f37
Jenkibear <jenkinwork123@gmail.com>
2024-04-21 12:04:25 +1000

    half of the context methods add and update

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -2,14 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
+  const [todos,setTodos]= useState([]);
+
+  function addTodo(todo){
+    setTodos((prev)=>[{id:Date.now(), ...todo},[...prev]])
+
+  }
+
+  function updateTodo(id,todo){
+    setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id===todo.id?todo:prevTodo)))
+  }
+
+  
 
 
   return (
-    <>
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
       <h1 className='text-3xl font-bold underline'>dasadhabsukdbajkdfbckasfcsd</h1>
-    </>
+    </TodoProvider>
   )
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # projectbasedlearning-react
 
-////// solid 6 hour mark 32 min
+////// solid 6 hour mark 50:47 min
