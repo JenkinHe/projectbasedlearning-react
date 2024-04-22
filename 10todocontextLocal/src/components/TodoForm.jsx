e45e7e7b21b6cfb33dc13f879f33d7ba56c368a3
Jenkibear <jenkinwork123@gmail.com>
2024-04-22 20:06:19 +1000

    overhaul but still cant find problem

--- 10todocontextLocal/src/components/TodoForm.jsx
+++ 10todocontextLocal/src/components/TodoForm.jsx
@@ -1,27 +1,32 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useState } from 'react'
 import {useTodo} from '../contexts/ToDoContext'
 
-export default function TodoForm() {
-    const [todo,setTodo]=useState('')
-    const {addTodo}= useTodo()
+function TodoForm() {
+    const [todo, setTodo] = useState("")
+    const {addTodo} = useTodo()
 
-    function add(e){
+    const add = (e) => {
         e.preventDefault()
-        if(!todo) return;
-        addTodo({todo,completed:false})
-        setTodo('')
+        if (!todo) return
+        addTodo({todo, completed: false})
+        setTodo("")
     }
-
   return (
     <form onSubmit={add} className='flex'>
-        <input type='text'
-        placeholder='Write todo'
-        className=' w-full border border-black/10 rounded-l-lg px-3 outline duration-150 bg-white/20 py-1.5'
+        <input type="text" 
+        placeholder='Write Todo...'
+        className='w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5'
         value={todo}
-        onChange={(e)=>setTodo(e.target.value)}
+        onChange={(e) => setTodo(e.target.value)}
         />
-        <button type='submit'
-        className='rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0'> Add</button>
+        <button type="submit"
+        className='rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0'
+        >Add</button>
     </form>
   )
 }
+
+export default TodoForm
+
+
