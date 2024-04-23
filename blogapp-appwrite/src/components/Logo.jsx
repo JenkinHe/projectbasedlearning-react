5d27f34c63f7152e4f22f19626321fd6762d7e47
Jenkibear <jenkinwork123@gmail.com>
2024-04-23 17:33:05 +1000

    Logo design

---
+++ blogapp-appwrite/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logo({width='100%'}) {
+  return (
+    <img src='
+    https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcR8YpxUI073E3npAfwtvHTdRV8b8cNX62MwhugDB-45nMhJ7zhY2slGX00Yme7Lh00UGghNoGl3FO544HY
+    ' style={{width}} alt='Logo PlaceHolder'/>
+  )
+}
