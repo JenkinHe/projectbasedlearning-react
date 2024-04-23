9a0bbf49f10c74e8d0226e5b0b27c3c19550418f
Jenkibear <jenkinwork123@gmail.com>
2024-04-23 15:12:02 +1000

    create post method

--- blogapp-appwrite/src/appwrite/config.js
+++ blogapp-appwrite/src/appwrite/config.js
@@ -35,6 +35,17 @@ export class Service{
         }
 
     }
+
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            return await this.databases.createDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,
+            slug, {title,content,featuredImage,status,userId})
+            
+        } catch (error) {
+            console.log('Appwrite service ::createPost()::',error);
+            return false;
+        }
+    }
 }
 
 
